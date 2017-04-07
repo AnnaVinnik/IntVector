f0336b2b01c17c2078d31917ef81e8d7c1fdeeeb
AnnaVinnik <vinnik.anna@rambler.ru>
2017-04-07 16:19:26 +0700

    push back

--- IntVector.c
+++ IntVector.c
@@ -64,3 +64,15 @@ size_t int_vector_get_capacity(const IntVector *v){
 	if (v)
 	return v->capacity;
 }
+
+int int_vector_push_back(IntVector *v, int item){
+	if (v){
+	if (v->size == v->capacity){
+		v->capacity = v->capacity*2;
+		v->data = realloc(v->data, (sizeof(int)*v->capacity));
+		}
+	v->data[v->size] = item;
+	v->size = v->size + 1;
+	}
+return 0;
+}
--- main.c
+++ main.c
@@ -22,6 +22,19 @@ int main(){
 
 	size_t cap = int_vector_get_capacity(v);
 	printf("capacity: %ld \n", cap);
+	
+	int_vector_push_back(v, 5);
+	printf("push size: %ld capacity: %ld elem:  %d \n", v->size, v->capacity, v->data[size]);
+	
+	int_vector_push_back(v, 6);
+	printf("push size: %ld capacity: %ld elem:  %d \n", v->size, v->capacity, v->data[size]);
+
+	int_vector_push_back(v, 3);
+	printf("push size: %ld capacity: %ld elem:  %d \n", v->size, v->capacity, v->data[size]);
+
+	int_vector_push_back(v, 1);
+	printf("push size: %ld capacity: %ld elem:  %d \n", v->size, v->capacity, v->data[size]);
+	
 
 return 0;
 }
